65b6fe00e9caf34b0dadc40f60fd8acb3ce9e53f
GUSTAVO HIROSHI FUKUDA <62963209+fukudaum@users.noreply.github.com>
2024-06-18 19:12:06 -0300

    add messages module

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { MessagesService } from './messages.service';
+import { MessagesController } from './messages.controller';
 
-@Module({})
+@Module({
+  providers: [MessagesService, PrismaService],
+  controllers: [MessagesController],
+})
 export class MessagesModule {}
