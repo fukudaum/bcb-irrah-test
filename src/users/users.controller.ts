e45ce8d583d3a0964791daeab5fba29351effdc9
GUSTAVO HIROSHI FUKUDA <62963209+fukudaum@users.noreply.github.com>
2024-06-18 19:11:55 -0300

    Add backoffice routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,9 @@ import {
   Body,
   Controller,
   Delete,
+  Get,
   Param,
+  Patch,
   Post,
   Put,
   UseGuards,
@@ -10,6 +12,7 @@ import {
 import { UsersService } from './users.service';
 import { Prisma, User } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UpdateBalanceDto, UpdateLimitDto, UpdatePlanDto } from './users.dto';
 
 @Controller('users')
 export class UsersController {
@@ -38,11 +41,59 @@ export class UsersController {
     }
   }
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   async createUser(@Body() params: any): Promise<User> {
     try {
       return await this.usersService.createUser(params);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/:userId')
+  async getUser(@Param('userId') userId: number) {
+    try {
+      return await this.usersService.findUserById(userId);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  @UseGuards(JwtAuthGuard)
+  @Patch('/backoffice/plan/:plan/:userId')
+  async updatePlan(@Param() params: UpdatePlanDto) {
+    try {
+      return await this.usersService.updatePlan(params);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('/backoffice/limit/:limit/:userId')
+  async updateLimit(@Param() params: UpdateLimitDto) {
+    try {
+      return await this.usersService.updateLimit(params);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('/backoffice/balance/:balance/:userId')
+  async updateBalance(@Param() params: UpdateBalanceDto) {
+    try {
+      return await this.usersService.addBalance(params);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('/backoffice/balance/:userId')
+  async getBalance(@Param('useId') userId: number) {
+    try {
+      return await this.usersService.getBalance(+userId);
     } catch (error) {
       console.error(error);
     }
